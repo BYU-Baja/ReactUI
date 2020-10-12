62dd6fcb2e573a8b658cc46855ab3f7cb5428d05
Justin Miller <justinamiller1023@gmail.com>
2020-10-12 08:59:39 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, Fragment } from 'react';
+import './App.css';
+import bajaLogo from '/Users/justinmiller/taste_of_mqtt_in_react/src/BajaLogo.png';
+
+var mqtt    = require('mqtt');
+var client  = mqtt.connect('mqtt://24.10.232.182:1883');
+
+// preciouschicken.com is the MQTT topic
+client.subscribe('baja/sensors/#');
+
+function App() {
+  var note;
+  client.on('message', function (topic, message) {
+    note = message.toString();
+    // Updates React state with message 
+    setMesg(note);
+    console.log(note);
+    client.end();
+    });
+
+  // Sets default React state 
+  const [mesg, setMesg] = useState(<Fragment><em>nothing published</em></Fragment>);
+
+  return (
+    <div className="App">
+    <header className="App-header">
+    <img src={bajaLogo} className="App-logo" alt="logo" />
+    <h1>BYU BAJA Base Station</h1>
+    <p>Incoming Data: {mesg}</p>
+		<p>
+		</p>
+		</header>
+		</div>
+  );
+}
+
+export default App;
