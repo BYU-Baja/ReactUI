2e763ad39956439dca14fa5175ad0e0d6bdb8e2b
Ben Brenkman <justbrenkman@gmail.com>
2020-10-20 10:12:33 -0600

    updated map with icons

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import bajaLogo from "../src/BajaLogo.png";
 import VehicleMap from "./components/Map/index";
 
 var mqtt = require("mqtt");
-var client = mqtt.connect("mqtt://127.0.0.1:8883");
-
-// preciouschicken.com is the MQTT topic
-client.subscribe("baja/sensors/#");
+var client = mqtt.connect("mqtt://24.10.232.182:8883");
 
 client.on("connect", function () {
+  client.subscribe("baja/sensors/#");
   client.subscribe("presence", function (err) {
     if (!err) {
       client.publish("presence", "Hello mqtt");
@@ -39,7 +37,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <VehicleMap></VehicleMap>
+        <VehicleMap vehicleLocation={[-111.6672998, 40.2669074]} baseLocation={[-111.6472998, 40.2469074]} mapCenterStart={{lat: 40.2469074, lng: -111.6472998, zoom: 15}}></VehicleMap>
         <img src={bajaLogo} className="App-logo" alt="logo" />
         <h1>BYU BAJA Base Station</h1>
         <p>Incoming Data: {mesg}</p>
--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,34 +1,58 @@
 import React, { Component } from "react";
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import ReactMapboxGl, { Layer, Feature, Image } from 'react-mapbox-gl';
 import "./style.css";
+import baja_vehicle from "../../assets/motorbike_32.png";
+import antenna from "../../assets/antenna_32.png"
 
 const Map = ReactMapboxGl({
   accessToken:
     'pk.eyJ1IjoianVzdGJyZW5rbWFuIiwiYSI6ImNrYXI5ZXNhazBjbW8yem14ZDR5Y2p5a2wifQ.rVv3z5OQ8hrH61I4y9qDyw'
 });
 
-export default class VehicleMap extends React.Component {
+export default class VehicleMap extends Component {
   constructor(props) {
     super(props);
+    console.log(props);
+    this.style = props.style;
     this.state = {
-      lng: 10,
-      lat: 10,
-      zoom: 10
+      centerMap: {
+        lng: props.mapCenterStart.lng || 0,
+        lat: props.mapCenterStart.lat || 0,
+        zoom: props.mapCenterStart.zoom || 10
+      }
     };
+    
   }
 
-  style_blueprint = "mapbox://styles/justbrenkman/ckgbjgjuf055519oa5ybhx06g";
+
   style_dark = "mapbox://styles/mapbox/dark-v10";
   style_streets = "mapbox://styles/mapbox/streets-v11";
   style_outdoors = "mapbox://styles/mapbox/outdoors-v11";
+  style_satellite = 'mapbox://styles/mapbox/satellite-v9';
+  style_blueprint = "mapbox://styles/justbrenkman/ckgbjgjuf055519oa5ybhx06g";
 
+  defaultVehicleLocation = this.props.vehicleLocation || [0, 0];
+  defaultBaseLocation = this.props.baseLocation || [0, 0];
 
   render() {
     return <div className="map_container">
-        <Map style={this.style_blueprint} containerStyle={{height: '50vh',width: '50vw', borderRadius: 64, boxShadow: '5px 5px 20px black'}}>
+        <Map center={[this.state.centerMap.lng, this.state.centerMap.lat]} style={this.style_streets} containerStyle={{height: '50vh',width: '50vw', borderRadius: 64, boxShadow: '5px 5px 20px black'}} onMove={(map, _) => {this.setState({centerMap: {lat: map.getCenter().lat, lng: map.getCenter().lng, zoom: map.getZoom()}})}}>
           <div className='sidebarStyle'>
-            <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+            <div>Longitude: {this.state.centerMap.lng} | Latitude: {this.state.centerMap.lat} | Zoom: {this.state.centerMap.zoom}</div>
           </div>
+          <Image id={"baja_vehicle"} url={baja_vehicle}/>
+          <Image id={"antenna"} url={antenna}/>
+          <Layer
+            type="symbol"
+            layout={{ "icon-image": "baja_vehicle", 'icon-size': 1, 'icon-allow-overlap': true }}>
+              <Feature coordinates={this.defaultVehicleLocation} />
+          </Layer>
+
+          <Layer
+            type="symbol"
+            layout={{ "icon-image": "antenna", 'icon-size': 1, 'icon-allow-overlap': true }}>
+              <Feature coordinates={this.defaultBaseLocation} />
+          </Layer>
         </Map>
       </div>;
   };
