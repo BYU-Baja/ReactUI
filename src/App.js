26d85c8359553e21899b1c189b1a9f9d14f2eaf3
Ben Brenkman <justbrenkman@gmail.com>
2021-03-09 09:34:56 -0700

    upated

--- src/App.js
+++ src/App.js
@@ -10,30 +10,8 @@ var client = mqtt.connect("mqtt://localhost:8883");
 
 client.on("connect", function () {
   // client.subscribe("#");
-  // client.subscribe("presence", function (err) {
-  //   if (!err) {
-  //     client.publish("presence", "Hello mqtt");
-  //     console.log("we did it!");
-  //   }
-  //   console.log(err);
-  // });
-  client.subscribe("baja/sensors/a3");  //front right rpm
-  client.subscribe("presence", function (err) {
-    if (!err) {
-      client.publish("presence", "Hello mqtt");
-      console.log("Front Right RPM sensor subscribed");
-      FRRPM();
-    }
-    console.log(err);
-  });
-  client.subscribe("baja/sensors/a4");  //front left rpm
-  client.subscribe("presence", function (err) {
-    if (!err) {
-      client.publish("presence", "Hello mqtt");
-      console.log("Front left RPM sensor subscribed");
-    }
-    console.log(err);
-  });
+  client.subscribe("baja/sensors/a3"); //front right rpm
+  client.subscribe("baja/sensors/a4"); //front left rpm
 });
 
 //var data =  [64, 226, 157, 10];
@@ -45,38 +23,35 @@ client.on("connect", function () {
 var frrpm = 0.0;
 var flrpm = 0.0;
 
-function FRRPM(data){
+function FRRPM(data) {
+  // console.log(data);
   var buf = new ArrayBuffer(4);
   var view = new DataView(buf);
   data.forEach(function (b, i) {
     view.setUint8(i, b);
   });
-  frrpm = view.getFloat32(0); 
-};
+  frrpm = view.getFloat32(0);
+  // console.log(frrpm);
+}
 
-function FLRPM(data){
+function FLRPM(data) {
   var buf = new ArrayBuffer(4);
   var view = new DataView(buf);
   data.forEach(function (b, i) {
     view.setUint8(i, b);
   });
-  flrpm = view.getFloat32(0); 
-};
+  flrpm = view.getFloat32(0);
+}
 
 function App() {
   var note;
   client.on("message", function (topic, message) {
-    note = message.toString("hex");
     // Updates React state with message
-    if(topic === "baja/sensors/a4"){
+    if (topic == "baja/sensors/a4") {
       FRRPM(message);
-    }
-    else if(topic === "baja/sensors/a3"){
+    } else if (topic === "baja/sensors/a3") {
       FLRPM(message);
     }
-    setMesg(note);
-    console.log(note);
-    client.end();
   });
 
   // Sets default React state
@@ -92,34 +67,34 @@ function App() {
   let throtle = 65;
 
   const AlignLeft = styled.div`
-  width: 60%;
-  height: 75%;
-  position: absolute;
-  left:0;
-  bottom:0;
-`;
+    width: 60%;
+    height: 75%;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+  `;
 
   const AlightRight = styled.div`
     width: 23%;
     height: 78%;
     position: absolute;
-    Right:0;
-    bottom:0;
+    right: 0;
+    bottom: 0;
   `;
 
   const AlightRight2 = styled.div`
     width: 67%;
     height: 78%;
     position: absolute;
-    Right:0;
-    bottom:0;
+    right: 0;
+    bottom: 0;
   `;
 
   const AlightTop = styled.div`
     width: 100%;
     height: 17%;
     position: absolute;
-    top:20px;
+    top: 20px;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -127,18 +102,18 @@ function App() {
   `;
 
   const CenterJustify = styled.div`
-  //min-height: 120vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
+    //min-height: 120vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
   `;
 
   const ButtonRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
   `;
 
   const DisplayStle = styled.div`
@@ -153,40 +128,79 @@ function App() {
     background-color: #282c34;
   `;
 
-  function clicked(){
-    alert("You clicked me")
-  };
+  function clicked() {
+    alert("You clicked me");
+  }
 
-  function fuelClick(){
+  function fuelClick() {
     milesRemaining = 100;
     alert("miles remaining: " + milesRemaining);
     //state.setState({ mssg: "Hi there!" });
-  };
+  }
 
   return (
     <ColorDiv>
       <AlightTop>
-      <img src={bajaLogo} className="App-logo" alt="logo" />
+        <img src={bajaLogo} className="App-logo" alt="logo" />
       </AlightTop>
-      <DisplayStle><AlignLeft>
-      <div className="App">
-      <header className="App-header2">
-      <p></p>
-      <VehicleMap vehicleLocation={{lng: -111.6672998, lat: 40.2669074}} baseLocation={{lng: -111.6472998, lat: 40.2469074}} mapCenterStart={{lat: 40.2469074, lng: -111.6472998, zoom: 15}} vehicleHistory={[{lng: -111.6472998, lat: 40.2469074}, {lng: -111.6672998, lat: 40.2669074}]}></VehicleMap>
-      <p></p>
-      <ButtonRow><button onClick = {clicked} style = {{margin: 20}}>Set Base Station Location</button><button onClick = {fuelClick} style = {{margin: 20}}>Fuel Tank Filled</button></ButtonRow>
-      </header>
-      </div></AlignLeft>
-      <div className = "App"><header className = "App-header">
-      <AlightRight><CenterJustify>
-        <DataComp dataType = "MPH" dataNum = {mesg.toString()}></DataComp><DataComp dataType = "RPM FR(x1000)" dataNum = {frrpm.toFixed(1)}></DataComp><DataComp dataType = "Gallons Remain" dataNum = {milesRemaining}></DataComp>
-        </CenterJustify></AlightRight>
-      <AlightRight2><CenterJustify>
-      <DataComp dataType = "Throttle (%)" dataNum = {throtle}></DataComp><DataComp dataType = "RPM FL(x1000)" dataNum = {flrpm.toFixed(1)}></DataComp><DataComp dataType = "RPM (x1000)" dataNum = {rpm}></DataComp>
-      </CenterJustify></AlightRight2>
-      </header>
-      </div>
-      </DisplayStle> 
+      <DisplayStle>
+        <AlignLeft>
+          <div className="App">
+            <header className="App-header2">
+              <p></p>
+              <VehicleMap
+                vehicleLocation={{ lng: -111.6672998, lat: 40.2669074 }}
+                baseLocation={{ lng: -111.6472998, lat: 40.2469074 }}
+                mapCenterStart={{
+                  lat: 40.2469074,
+                  lng: -111.6472998,
+                  zoom: 15,
+                }}
+                vehicleHistory={[
+                  { lng: -111.6472998, lat: 40.2469074 },
+                  { lng: -111.6672998, lat: 40.2669074 },
+                ]}
+              ></VehicleMap>
+              <p></p>
+              <ButtonRow>
+                <button onClick={clicked} style={{ margin: 20 }}>
+                  Set Base Station Location
+                </button>
+                <button onClick={fuelClick} style={{ margin: 20 }}>
+                  Fuel Tank Filled
+                </button>
+              </ButtonRow>
+            </header>
+          </div>
+        </AlignLeft>
+        <div className="App">
+          <header className="App-header">
+            <AlightRight>
+              <CenterJustify>
+                <DataComp dataType="MPH" dataNum={mesg.toString()}></DataComp>
+                <DataComp
+                  dataType="RPM FR(x1000)"
+                  dataNum={frrpm.toFixed(1)}
+                ></DataComp>
+                <DataComp
+                  dataType="Gallons Remain"
+                  dataNum={milesRemaining}
+                ></DataComp>
+              </CenterJustify>
+            </AlightRight>
+            <AlightRight2>
+              <CenterJustify>
+                <DataComp dataType="Throttle (%)" dataNum={throtle}></DataComp>
+                <DataComp
+                  dataType="RPM FL(x1000)"
+                  dataNum={flrpm.toFixed(1)}
+                ></DataComp>
+                <DataComp dataType="RPM (x1000)" dataNum={rpm}></DataComp>
+              </CenterJustify>
+            </AlightRight2>
+          </header>
+        </div>
+      </DisplayStle>
     </ColorDiv>
   );
 }
